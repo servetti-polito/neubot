5a2943b8cb3fd60f1d0dfa98aab709b20f66de6e
Simone Basso <bassosimone@gmail.com>
2013-03-19 19:49:50 +0100

    add M-Lab/uninstall.sh: script to remove old neubot

---
+++ M-Lab/uninstall.sh
@@ -0,0 +1,40 @@
+#!/bin/sh -e
+
+#
+# Copyright (c) 2013
+#     Nexa Center for Internet & Society, Politecnico di Torino (DAUIN)
+#     and Simone Basso <bassosimone@gmail.com>
+#
+# This file is part of Neubot <http://www.neubot.org/>.
+#
+# Neubot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Neubot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
+# Uninstall Neubot from M-Lab sliver - Invoked on the sliver
+# by init/initialize.sh.
+#
+# I want this script to run before installing a new Neubot to
+# make sure that the previous deployment is removed: old files
+# still around may cause unpredictable behavior.
+#
+# I don't remove the user and the group, though, because I
+# never uninstall Neubot from M-Lab without immediately installing
+# a newer version.
+#
+
+DEBUG=
+$DEBUG echo "remove old neubot: init/ neubot/ version"
+$DEBUG rm -rf /home/mlab_neubot/init /home/mlab_neubot/neubot \
+              /home/mlab_neubot/version
